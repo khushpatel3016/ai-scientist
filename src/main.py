bd4e1e8ac3f64f3ca3adef3c9a6c13b50902ae31
Khush Patel <khushpatel3016@gmail.com>
2026-03-25 21:54:15 +0530

    updated the MAIN file for the PIPELINE

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
-from reader import extract_text_from_pdf
-from summarizer import summarize_text
+from reader import extract_text_from_pdf, split_text
+from embedder import get_embeddings
+from retriever import create_index, search
+from qa import generate_answer
 
 pdf_path = "data/sample.pdf"
 
 print("Reading PDF...")
 text = extract_text_from_pdf(pdf_path)
 
-print("Summarizing...")
-summary = summarize_text(text)
+chunks = split_text(text)
 
-print("\n--- FINAL SUMMARY ---\n")
-print(summary)
+print("Creating embeddings...")
+embeddings = get_embeddings(chunks)
+
+index = create_index(embeddings)
+
+question = input("Ask a Question: ")
+
+query_embedding = get_embeddings([question])
+
+indices = search(index, query_embedding)
+
+context = " ".join([chunks[i] for i in indices])
+
+answer = generate_answer(context, question)
+
+print("\nAnswer:\n", answer)
